b962625afa52a04b734d842e7e3d4565badf229f
Regan-Mclachlan <reganmc96@hotmail.com>
2021-08-27 19:36:10 +1000

    still trying to get sql to qork on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_224509) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "groups", force: :cascade do |t|
-    t.string "group_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "task_groups", force: :cascade do |t|
-    t.integer "task_id", null: false
-    t.integer "group_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["group_id"], name: "index_task_groups_on_group_id"
-    t.index ["task_id"], name: "index_task_groups_on_task_id"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "status"
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "user_groups", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "group_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["group_id"], name: "index_user_groups_on_group_id"
-    t.index ["user_id"], name: "index_user_groups_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "provider"
-    t.string "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "task_groups", "groups"
-  add_foreign_key "task_groups", "tasks"
-  add_foreign_key "user_groups", "groups"
-  add_foreign_key "user_groups", "users"
 end
